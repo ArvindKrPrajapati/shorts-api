c4c5ed83cbac7d9c07dc908294754b6186d68df7
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-06-24 20:14:02 +0530

    profile api added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,12 +6,16 @@ const getUserById = async (req, res) => {
         if (!_id) {
             return res.status(404).json({ success: false, message: "id is not provided" })
         }
-        _id = mongoose.Types.ObjectId(_id)
-        const data = await user.findOne({ _id }).select('_id name desc image')
-        if(!data){
-            return res.status(404).json({success:false,message:'user not found'})
+        if (mongoose.Types.ObjectId.isValid(_id)) {
+            _id = mongoose.Types.ObjectId(_id)
+            const data = await user.findOne({ _id }).select('_id name desc image')
+            if (!data) {
+                return res.status(404).json({ success: false, message: 'user not found' })
+            }
+            return res.status(200).json({ success: true, data })
+        } else {
+            return res.status(401).json({ success: false, message: "invalid id" })
         }
-        res.status(200).json({ success: true, data })
     } catch (error) {
         return res.status(500).json({ success: false, message: "server error" })
     }
