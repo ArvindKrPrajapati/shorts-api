c982080754afaee8ee00488521923d4fe892f987
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-06-26 17:09:27 +0530

    updated

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -18,7 +18,7 @@ const createPost = async (req, res) => {
 const myPosts = async (req, res) => {
     try {
         let _id = req.query.id
-        const skip=Number(req.query.skip) || 0
+        const skip = Number(req.query.skip) || 0
         if (!_id) {
             return res.status(404).json({ success: false, message: "id is not provided" })
         }
@@ -27,8 +27,8 @@ const myPosts = async (req, res) => {
             const data = await post.aggregate([
                 { $match: { postedby: _id } },
                 { $sort: { datetime: -1 } },
-                {$skip:skip},
-                {$limit:8},
+                { $skip: skip },
+                { $limit: 8 },
                 {
                     $project: {
                         url: 1,
@@ -37,7 +37,7 @@ const myPosts = async (req, res) => {
                         comments: { $cond: { if: { $isArray: "$comments" }, then: { $size: "$comments" }, else: 0 } }
                     }
                 },
-             
+
 
             ])
             return res.status(200).json({ success: true, data })
@@ -45,27 +45,68 @@ const myPosts = async (req, res) => {
             return res.status(401).json({ success: false, message: "invalid id" })
         }
     } catch (error) {
-        console.log(error);
         return res.status(500).json({ success: false, message: "server error" })
     }
 }
 
+const react = async (req, res) => {
+    try {
+        const { postid, like } = req.body
+        if (!postid) {
+            return res.status(404).json({ success: false, message: "postid is not provided" })
+        }
+        if (mongoose.Types.ObjectId.isValid(postid)) {
+            const _id = mongoose.Types.ObjectId(postid)
+            if (like) {
+                const data=await post.findByIdAndUpdate(_id,{$push:{likes:{by:req.userid}}},{new:true})
+                return res.status(200).json({ success: true, data:"liked" })
+            } else {
+                const data=await post.findByIdAndUpdate(_id,{$pull:{likes:{by:req.userid}}},{new:true})
+                return res.status(200).json({ success: true, data:"disliked" })
+            }
+        } else {
+            return res.status(401).json({ success: false, message: "invalid postid" })
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "server error" })
+    }
+}
+
+const createComment = async (req, res) => {
+    try {
+        const { postid, comm } = req.body
+        if (!postid && !comm) {
+            return res.status(404).json({ success: false, message: "postid or comm is not provided" })
+        }
+        if (mongoose.Types.ObjectId.isValid(postid)) {
+            const _id = mongoose.Types.ObjectId(postid)
+                const data=await post.findByIdAndUpdate(_id,{$push:{comments:{by:req.userid,comm}}},{new:true})
+                return res.status(200).json({ success: true, data:"your comment is added" })
+        } else {
+            return res.status(401).json({ success: false, message: "invalid postid" })
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "server error" })
+    }
+}
 
 const getAllPost = async (req, res) => {
     try {
-        const data =await post.aggregate([
+        const data = await post.aggregate([
             { $lookup: { from: 'users', localField: 'postedby', foreignField: '_id', as: 'postedby' } },
-            {$unwind:'$postedby'},
-            {$project:{
-                url:1,
-                desc:1,
-                postedby:{_id:1,name:1,image:1},
-                likes:{$cond:{if:{$isArray:"$likes"},then:{$size:"$likes"},else:0}},
-                comments:{$cond:{if:{$isArray:"$comments"},then:{$size:"$comments"},else:0}},
-            }},
-            {$addFields:{isLiked:false}}
+            { $unwind: '$postedby' },
+            {
+                $project: {
+                    url: 1,
+                    desc: 1,
+                    postedby: { _id: 1, name: 1, image: 1 },
+                    likes: { $cond: { if: { $isArray: "$likes" }, then: { $size: "$likes" }, else: 0 } },
+                    comments: { $cond: { if: { $isArray: "$comments" }, then: { $size: "$comments" }, else: 0 } },
+                }
+            },
+            { $addFields: { isLiked: false } }
         ])
-    //  const data = await post.find({}).populate("postedby", "_id name image").select("-__v")
+        //  const data = await post.find({}).populate("postedby", "_id name image").select("-__v")
         res.status(200).json({ success: true, data })
     } catch (error) {
         console.log(error)
@@ -74,7 +115,7 @@ const getAllPost = async (req, res) => {
 }
 
 module.exports = {
-    createPost, getAllPost, myPosts
+    createPost, getAllPost, myPosts, react,createComment
 }
 
 
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const postroute=express.Router()
-const { createPost, getAllPost, myPosts } = require('../controllers/post.controller')
+const { createPost, getAllPost, myPosts, react, createComment } = require('../controllers/post.controller')
 postroute.post("/create",createPost)
 postroute.get("/all",getAllPost)
 postroute.get("/mypost",myPosts)
+postroute.post("/react",react)
+postroute.route("/comment").post(createComment)
 module.exports=postroute
