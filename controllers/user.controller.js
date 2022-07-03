0acfa0b48f76f0199077a5fc1624324c0e1ef54a
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-07-03 11:37:45 +0530

    updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,6 @@
 const user = require("../modals/user.modal")
+const post = require("../modals/post.modal")
+const follow = require("../modals/follow.modal")
 const mongoose = require('mongoose');
 const getUserById = async (req, res) => {
     try {
@@ -8,15 +10,25 @@ const getUserById = async (req, res) => {
         }
         if (mongoose.Types.ObjectId.isValid(_id)) {
             _id = mongoose.Types.ObjectId(_id)
-            const data = await user.findOne({ _id }).select('_id name desc image')
-            if (!data) {
+            const userInfo = await user.findOne({ _id }).select('_id name desc image')
+            if (!userInfo) {
                 return res.status(404).json({ success: false, message: 'user not found' })
             }
-            return res.status(200).json({ success: true, data })
+            const shorts =await post.find({postedby:_id}).count()
+            const ifollow = await follow.findOne({to:_id,by:req.userid}).count()
+            const followers=await follow.find({to:_id}).count()
+            const followings=await follow.find({by:_id}).count()
+            
+            return res.status(200).json({ success: true, data:{
+                name:userInfo.name,_id:userInfo._id,desc:userInfo.desc,image:userInfo.image,
+                shorts:shorts,ifollow:ifollow==0 ? false : true,
+                followers,followings
+            } })
         } else {
             return res.status(401).json({ success: false, message: "invalid id" })
         }
     } catch (error) {
+        console.log(error);
         return res.status(500).json({ success: false, message: "server error" })
     }
 }
